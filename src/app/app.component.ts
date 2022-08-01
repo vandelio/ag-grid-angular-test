160d2715357faac05d3b508c3b3622d5785b3666
vandelio <stefanvandel@gmail.com>
2022-08-01 19:38:22 +0200

    2 files modified, 2 added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,20 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, ViewChild } from '@angular/core';
-import { AgGridAngular } from 'ag-grid-angular';
-import { CellClickedEvent, ColDef, GridReadyEvent } from 'ag-grid-community';
 import { Observable } from 'rxjs';
+
+import { AgGridAngular } from 'ag-grid-angular';
+import {
+  CellClickedEvent,
+  ColDef,
+  ColumnApi,
+  ColumnResizedEvent,
+  GridApi,
+  GridReadyEvent,
+} from 'ag-grid-community';
+import 'ag-grid-community/styles/ag-grid.css';
+import 'ag-grid-community/styles/ag-theme-alpine.css';
+
+import { ICarData } from './interfaces';
 
 @Component({
   selector: 'my-app',
@@ -10,31 +22,66 @@ import { Observable } from 'rxjs';
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
+  private gridApi!: GridApi;
+  private gridColumnApi!: ColumnApi;
+
   // Each Column Definition results in one Column.
   public columnDefs: ColDef[] = [
-    { field: 'make' },
-    { field: 'model' },
-    { field: 'price' },
+    { field: 'name' },
+    { field: 'company' },
+    { field: 'email' },
+    { field: 'phone' },
+    {
+      field: 'about',
+      resizable: true,
+      minWidth: 200,
+      maxWidth: 500,
+    },
+    {
+      headerName: 'Friend count', // Specific column name, when doing custom change on specific column value
+      field: 'friends', // Object property and source of data
+      valueFormatter: this.friendsFormatter, // Formatting displayed value
+    },
   ];
   // DefaultColDef sets props common to all Columns
   public defaultColDef: ColDef = {
     sortable: true,
     filter: true,
+    resizable: true,
   };
 
-  // Data that gets displayed in the grid
-  public rowData$!: Observable<any[]>;
+  public rowData!: ICarData[];
+  //rowData: any = [];
 
   //for accesing the grid's API
   @ViewChild(AgGridAngular) agGrid!: AgGridAngular;
 
   constructor(private http: HttpClient) {}
 
-  // Example load data from sever
-  onGridReady(params: GridReadyEvent) {
-    this.rowData$ = this.http.get<any[]>(
-      'https://www.ag-grid.com/example-assets/row-data.json'
-    );
+  // On grid ready we get and then assign the response data to the variable used in the chart
+  onGridReady(params: GridReadyEvent<ICarData>) {
+    this.gridApi = params.api;
+    this.gridColumnApi = params.columnApi;
+    this.http
+      .get<ICarData[]>(
+        'https://ag-grid-angular-hello-world-bwvk8k.stackblitz.io/assets/cardata.json'
+      )
+      .subscribe((data) => {
+        this.rowData = data;
+      });
+  }
+
+  /// Resize columns functions
+  autoSizeAll(skipHeader: boolean) {
+    const allColumnIds: string[] = [];
+    this.gridColumnApi.getColumns()!.forEach((column) => {
+      allColumnIds.push(column.getId());
+    });
+    this.gridColumnApi.autoSizeColumns(allColumnIds, skipHeader);
+  }
+
+  sizeToFit() {
+    this.gridApi.sizeColumnsToFit();
   }
 
   // Example of consuming Grid Event
@@ -45,5 +92,9 @@ export class AppComponent {
   // Example using Grid's API
   clearSelection(): void {
     this.agGrid.api.deselectAll();
+  }
+
+  friendsFormatter(params) {
+    return params.value.length;
   }
 }
